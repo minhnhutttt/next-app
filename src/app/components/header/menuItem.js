0d46e6c888e7010b96350f0fa3589b41af71da0d
rabi-minhnhut <nhut@rabi.co.jp>
2023-09-07 22:46:52 +0700

    a

---
+++ src/app/components/header/menuItem.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import PropTypes from "prop-types";
+
+export default function MenuItem({link, children}) {
+  return (
+    <li>
+        <Link
+        className="text-[14px] xl:text-[16px] text-[#010101] uppercase" href={link}>
+        {children}
+        </Link>
+    </li>
+  );
+}
+
+MenuItem.propTypes = {
+    children: PropTypes.node.isRequired,
+    link: PropTypes.node.isRequired,
+  };
+  
