e5892d1d650b72b7c5c23ff7db45f4f44db251ac
rabi-minhnhut <nhut@rabi.co.jp>
2024-01-12 22:15:38 +0700

    a

---
+++ src/app/components/pages/introduction.js
@@ -0,0 +1,14 @@
+import SectionTitle from "./common/sectionTitle";
+
+export default function Introduction() {
+    return (
+        <div className="bg-[url('/images/background-introduction.png')] bg-cover bg-center px-5 md:py-20">
+            <div className="w-full max-w-[1280px] mx-auto h-screen">
+                <SectionTitle>
+                    新しい価値観、次世代を始めよう！<br />
+                    次世代アカウントだからできること
+                </SectionTitle>
+            </div>
+        </div>
+    )
+  }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import FV from "./components/pages/fv";
+import Introduction from "./components/pages/introduction";
 
 export default function Home() {
   return (
     <main>
       <FV />
+      <Introduction />
     </main>
   )
 }
