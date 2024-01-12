a7d5f1960e912fdcbde76923ab7cbec8ef778709
rabi-minhnhut <nhut@rabi.co.jp>
2024-01-12 17:40:45 +0700

    a

--- src/app/components/pages/home/experience.js
+++ src/app/components/pages/home/experience.js
@@ -18,17 +18,18 @@ export default function Experience() {
           <br />
           こんな経験、ありませんか？
         </h3>
-        <div className="mt-7 flex justify-center">
-          <div className="relative flex w-full justify-center py-[14vw] md:max-w-[850px] md:py-[88px]">
+        <div className="flex justify-center mt-7">
+          <div className="relative flex w-full justify-center py-5 md:max-w-[850px] md:py-[88px]">
             <figure
               ref={animateRefs}
-              className="ml-6 max-lg:w-[34vw] max-md:mt-[2vw]"
+              className="md:ml-6 max-md:w-[25vw] max-lg:w-[340px] max-md:mt-[2vw]"
             >
-              <img src="/images/image-experience.png" alt="" />
+              <img className="max-md:hidden" src="/images/image-experience.png" alt="" />
+              <img className="md:hidden" src="/images/image-experience-sp.png" alt="" />
             </figure>
             <div
               ref={animateLeftRefs}
-              className="absolute left-[4vw] top-0 flex h-[18vw] w-[30vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[5%] after:right-[8%]  after:block after:h-4 after:w-6 after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:left-[4%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:right-[3%] md:after:h-6 md:after:w-9 lg:text-[16px]"
+              className="absolute left-[4vw] md:top-0 top-[12vw] flex h-[20vw] w-[33vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[5%] after:right-[8%] after:block after:h-[4vw] after:w-[6vw] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:left-[4%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:right-[3%] md:after:h-6 md:after:w-9 lg:text-[16px]"
             >
               スマートフォンの
               <br />
@@ -36,7 +37,7 @@ export default function Experience() {
             </div>
             <div
               ref={animateLeftRefs}
-              className="absolute left-0 top-[19vw] flex h-[18vw] w-[30vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[12%] after:right-[2%] after:block  after:h-4 after:w-6 after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:left-[-1%] md:top-[31%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:right-[-2%] md:after:h-6 md:after:w-9 lg:text-[16px]"
+              className="absolute left-[1vw] top-[34vw] flex h-[20vw] w-[33vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[12%] after:right-[2%] after:block after:h-[4vw] after:w-[6vw] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:left-[-1%] md:top-[31%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:right-[-2%] md:after:h-6 md:after:w-9 lg:text-[16px]"
             >
               子どもの写真や動
               <br />
@@ -46,7 +47,7 @@ export default function Experience() {
             </div>
             <div
               ref={animateLeftRefs}
-              className="absolute left-[4%] top-[40vw] flex h-[18vw] w-[30vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[27%] after:right-[-4%] after:block after:h-4  after:w-6 after:rotate-[-30deg] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] max-md:rotate-[-16deg] md:left-[2%] md:top-[63%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:h-6 md:after:w-9 lg:text-[16px]"
+              className="absolute left-[3%] top-[57vw] flex h-[20vw] w-[33vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[27%] after:right-[-4%] after:block after:h-[4vw] after:w-[6vw] after:rotate-[-30deg] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:left-[2%] md:top-[63%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:h-6 md:after:w-9 lg:text-[16px]"
             >
               ストレージのサブス
               <br />
@@ -58,7 +59,7 @@ export default function Experience() {
             </div>
             <div
               ref={animateRightRefs}
-              className="absolute right-[3%] top-[10vw] flex h-[18vw] w-[30vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[14%] after:left-[0] after:block  after:h-4 after:w-6 after:rotate-[18deg] after:scale-x-[-1] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:right-[1.5%] md:top-[14%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:left-[-3%] md:after:h-6 md:after:w-9 lg:text-[16px]"
+              className="absolute right-[3%] top-[20vw] flex h-[20vw] w-[33vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[14%] after:left-[0] after:block after:h-[4vw] after:w-[6vw] after:rotate-[18deg] after:scale-x-[-1] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:right-[1.5%] md:top-[14%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:left-[-3%] md:after:h-6 md:after:w-9 lg:text-[16px]"
             >
               共有アプリにアップ
               <br />
@@ -70,7 +71,7 @@ export default function Experience() {
             </div>
             <div
               ref={animateRightRefs}
-              className="absolute right-[3%] top-[36vw] flex h-[18vw] w-[30vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[20%] after:left-[-3%] after:block after:h-4  after:w-6 after:rotate-[18deg] after:scale-x-[-1] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] max-md:rotate-[16deg] md:right-[1.5%] md:top-[54%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:h-6 md:after:w-9 lg:text-[16px]"
+              className="absolute right-[3%] top-[46vw] flex h-[20vw] w-[33vw] items-center justify-center rounded-[48%] border-2 border-[#231815] bg-white text-[2.2vw] font-bold after:absolute after:bottom-[20%] after:left-[-3%] after:block after:h-[4vw] after:w-[6vw] after:rotate-[18deg] after:scale-x-[-1] after:bg-[url(/images/chat.png)] after:bg-cover after:content-[''] md:right-[1.5%] md:top-[54%] md:h-[140px] md:w-[200px] md:text-[14px] md:after:h-6 md:after:w-9 lg:text-[16px]"
             >
               スマホの機種変更
               <br />
